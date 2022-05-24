839008034a188db336815dd09c99f1d753556be8
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-24 09:56:53 +0700

    implement arrive behaviour (slows down when near)

--- practice/vehicle.gd
+++ practice/vehicle.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 export var max_speed := 400.0
 export var max_force := 15.0
+export var slow_radius := 150
 
 var acceleration := Vector2.ZERO
 var velocity := Vector2.ZERO
@@ -27,9 +28,25 @@ func flee(target: Vector2):
 	acceleration += steer
 
 
+func arrive(target: Vector2):
+	var desired: Vector2 = target - position
+	var desired_length = desired.length()
+	desired = desired.normalized()
+	if desired_length < slow_radius:
+		var slow_speed = max_speed * (desired_length / slow_radius)
+		desired *= slow_speed
+	else:
+		desired *= max_speed
+
+	var steer: Vector2 = desired - velocity
+	steer = steer.clamped(max_speed)
+	acceleration += steer
+
+
 func _process(delta):
 	var target = get_global_mouse_position()
-	seek(target)
+	# seek(target)
+	arrive(target)
 	velocity += acceleration
 	velocity = velocity.clamped(max_speed)
 	look_at(position + velocity)
